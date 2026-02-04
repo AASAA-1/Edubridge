40bd485304b3de2989ec077289265a5c393eee68
unknown <faalqaahtani@gmail.com>
2026-02-04 14:42:40 +0300

    Update parent dashboard UI

--- app/src/main/java/com/example/edubridge/parent/StudentDashboardFragment.java
+++ app/src/main/java/com/example/edubridge/parent/StudentDashboardFragment.java
@@ -7,18 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.example.edubridge.R;
 
-public class ParentDashboardFragment extends Fragment {
+public class StudentDashboardFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_parent_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_student_dashboard, container, false);
 
-        v.findViewById(R.id.student_mode_btn).setOnClickListener(view -> {
+        v.findViewById(R.id.back_btn).setOnClickListener(view -> {
             requireActivity().getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container, new StudentDashboardFragment())
+                    .replace(R.id.fragment_container, new ParentDashboardFragment())
                     .commit();
         });
 
